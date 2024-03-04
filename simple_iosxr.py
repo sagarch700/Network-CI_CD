587fbf3fbba95e4e0131d9ea2ae4a9e6312a0179
Sagar Ch <sagarch700@gmail.com>
2024-03-05 00:43:13 +0530

    Building my CI/CD Pipeline: easy level

---
+++ simple_iosxr.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from jinja2.environment import Template
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.data import load_yaml
+from nornir_jinja2.plugins.tasks import template_file
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir.core.exceptions import NornirExecutionError
+
+# nr = InitNornir(config_file="config.yml")
+
+# def pull_info(task):
+#     result = task.run(task = napalm_get, getters = ["get_interfaces"])
+
+# results = nr.run(task = pull_info)
+
+
+nr = InitNornir(config_file= "config.yml")
+
+def pull_vars(task):
+    """
+    Pull host vars
+    """
+    result = task.run(task=load_yaml, file="group_vars/all_iosxr.yml")
+    task.host["facts"] = result.result
+    push_config(task)
+
+def push_config(task):
+    """
+    Push config changes
+    """
+    ospf_configs = task.run(task=template_file, template="ospf_iosxr.j2", path="templates")
+    configurations = ospf_configs.result.splitlines()
+    print(configurations)
+    task.run(task=send_configs, configs=configurations)
+
+results = nr.run(task=pull_vars)
+print_result(results)
+failures = nr.data.failed_hosts
+if failures:
+    raise NornirExecutionError("Nornir Failure Detected")
